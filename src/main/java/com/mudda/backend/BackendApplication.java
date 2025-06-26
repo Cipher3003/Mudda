cfe878fe36f57fdf7ceaec47fa748aa551ca27ed
ShubhPundir <shb.pndr@gmail.com>
2025-06-26 17:35:09 +0530

    postgresql files working as it should. MongoDB files are commented, revamp expected until 27/06/2025

--- src/main/java/com/mudda/backend/BackendApplication.java
+++ src/main/java/com/mudda/backend/BackendApplication.java
@@ -2,8 +2,11 @@ package com.mudda.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.mudda.backend.postgres.repositories") // Specify your repository package
+@SpringBootApplication()
 public class BackendApplication {
 
 	public static void main(String[] args) {
