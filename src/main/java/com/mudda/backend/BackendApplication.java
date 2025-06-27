544d3042105851b2a769735ac8d7c9e9d898b11b
ShubhPundir <shb.pndr@gmail.com>
2025-06-27 16:43:37 +0530

    @EnableMongoRepositories annotations added for extra back measures

--- src/main/java/com/mudda/backend/BackendApplication.java
+++ src/main/java/com/mudda/backend/BackendApplication.java
@@ -3,8 +3,10 @@ package com.mudda.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableJpaRepositories(basePackages = "com.mudda.backend.postgres.repositories") // Specify your repository package
+@EnableMongoRepositories(basePackages = "com.mudda.backend.mongodb.repositories")
 @SpringBootApplication()
 public class BackendApplication {
 
