75f26d69287cae38f1dda4bcd34a20b075652736
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2025-06-28 18:13:07 +0900

    Fix: added annotations on amazon image and repo

--- src/main/java/com/mudda/backend/amazon/models/AmazonImage.java
+++ src/main/java/com/mudda/backend/amazon/models/AmazonImage.java
@@ -1,10 +1,12 @@
 package com.mudda.backend.amazon.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@Entity
 public class AmazonImage {
 
     @Id
