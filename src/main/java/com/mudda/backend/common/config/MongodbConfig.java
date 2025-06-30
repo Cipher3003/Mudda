e47c552e3fa79ca8bbf5521eb751f5a7086e27c9
ShubhPundir <shb.pndr@gmail.com>
2025-06-30 16:55:21 +0530

    TODO: fix with proper mongodb template config
    Signed-off-by: ShubhPundir <shb.pndr@gmail.com>

--- src/main/java/com/mudda/backend/common/config/MongodbConfig.java
+++ src/main/java/com/mudda/backend/common/config/MongodbConfig.java
@@ -3,6 +3,9 @@ package com.mudda.backend.common.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//TODO
+// fix MongoDb Config, although this file itself isn't needed
+// do change it with corrected MongoTemplate
 @Configuration
 @EnableJpaRepositories(
         basePackages = "com.mudda.backend.mongodb.repository",
