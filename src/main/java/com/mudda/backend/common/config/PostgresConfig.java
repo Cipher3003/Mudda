5854587dc212d2956874cbc730bcd7c40e847f39
ShubhPundir <shb.pndr@gmail.com>
2025-07-02 13:39:48 +0530

    del: commented out code for MongodbConfig and PostgresConfig. Uncomment and add detailed end to end config only if annotations at main file fails to work or added services are required and/or clash with extra configurations
    Signed-off-by: ShubhPundir <shb.pndr@gmail.com>

--- src/main/java/com/mudda/backend/common/config/PostgresConfig.java
+++ src/main/java/com/mudda/backend/common/config/PostgresConfig.java
@@ -1,15 +1,17 @@
-package com.mudda.backend.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(
-        basePackages = "com.mudda.backend.postgres.repository",
-        entityManagerFactoryRef = "entityManagerFactory",
-        transactionManagerRef = "transactionManager"
-)
-public class PostgresConfig {
-    // Define DataSource, EntityManagerFactory, TransactionManager
-}
-
+//package com.mudda.backend.common.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//
+//@Configuration
+//@EnableJpaRepositories(
+//        basePackages = "com.mudda.backend.postgres.repository",
+//        entityManagerFactoryRef = "entityManagerFactory",
+//        transactionManagerRef = "transactionManager"
+//)
+//public class PostgresConfig {
+//    // Define DataSource, EntityManagerFactory, TransactionManager
+//}
+//
+ // TODO
+// only uncomment if the annotations at main file fails to work for added services
