a5e86101ec5371601cfe7adf9537f0d73e94d5e8
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2025-08-29 05:20:15 +0900

    Add: added swagger API

--- src/main/java/com/mudda/backend/BackendApplication.java
+++ src/main/java/com/mudda/backend/BackendApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Mudda API", version = "0.1.0", description = "API docs for Mudda Backend"))
 @EnableJpaRepositories(basePackages = {
 		"com.mudda.backend.postgres.repositories",
 		"com.mudda.backend.amazon.repositories"
