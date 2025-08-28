5ae3b0dc9892b5a724f970014d7f52ff750db9bd
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2025-08-29 04:10:30 +0900

    Fix: correct id annotation in vote

--- src/main/java/com/mudda/backend/postgres/models/Vote.java
+++ src/main/java/com/mudda/backend/postgres/models/Vote.java
@@ -2,12 +2,11 @@ package com.mudda.backend.postgres.models;
 
 import java.time.Instant;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
