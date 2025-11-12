2c2fd99221cbda455c1db59c837ce741a0ceac33
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2025-11-12 22:17:54 +0900

    Refactor: added more exceptions in exceptionHandler, remove postgresConfig

--- src/main/java/com/mudda/backend/utils/GlobalExceptionHandler.java
+++ src/main/java/com/mudda/backend/utils/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.mudda.backend.utils;
 
 import jakarta.persistence.EntityNotFoundException;
 
+import jakarta.persistence.OptimisticLockException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,8 +30,23 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(DuplicateResourceException.class)
-    public ResponseEntity<?> handleDuplicatEntity(DuplicateResourceException e) {
+    public ResponseEntity<?> handleDuplicateEntity(DuplicateResourceException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<?> handleNullPointerException(NullPointerException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+    @ExceptionHandler(OptimisticLockException.class)
+    public ResponseEntity<?> handleDatabaseLockingFailureException(OptimisticLockException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 
     // TODO: integrate messageUtil with this function properly
