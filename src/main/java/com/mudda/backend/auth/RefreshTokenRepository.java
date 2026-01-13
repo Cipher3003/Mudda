ce4b3650f0f82789e50ef9d916cfd464d74b8c2a
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2026-01-13 21:27:44 +0900

    Refactor: remove unused method in RefreshTokenRepository

--- src/main/java/com/mudda/backend/auth/RefreshTokenRepository.java
+++ src/main/java/com/mudda/backend/auth/RefreshTokenRepository.java
@@ -17,6 +17,4 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByTokenAndRevokedFalse(String token);
-
-    void deleteByUserId(Long userId);
 }
